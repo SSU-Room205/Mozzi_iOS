2efdf3bbff3d71a9e6091fc3e730a8f139cb9fcb
Zoe <Zoe.urssu@gmail.com>
2023-02-26 22:53:19 +0900

    Appicon
    앱아이콘 설정 및 기본 설정

---
+++ Mozzi/Mozzi/MainViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MainViewController.swift
+//  Mozzi
+//
+//  Created by 지희의 MAC on 2023/02/25.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Mozzi/Mozzi/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Mozzi
+//
+//  Created by 지희의 MAC on 2023/02/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
